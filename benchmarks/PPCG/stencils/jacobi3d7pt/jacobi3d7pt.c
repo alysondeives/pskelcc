8b6e8e23c8fca1e244d729b0e0810b4885b9f0f8
Alyson Deives <raziel363@gmail.com>
2017-08-17 23:10:32 -0300

    PPCG jacobi3d 7pt

--- benchmarks/PPCG/stencils/jacobi3d7pt/jacobi3d7pt.c
+++ benchmarks/PPCG/stencils/jacobi3d7pt/jacobi3d7pt.c
@@ -18,66 +18,78 @@
 #include <polybench.h>
 
 /* Include benchmark-specific header. */
-#include "jacobi2d.h"
+#include "../../common/common.h"
 
 
 /* Array initialization. */
 static
-void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+void init_array (int x,int y, int z,
+		 DATA_TYPE POLYBENCH_3D(A,X,Y,Z,x,y,z),
+		 DATA_TYPE POLYBENCH_3D(B,X,Y,Z,x,y,z))
 {
-  int i, j;
+  int i, j, k;
 
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
+  for (i = 0; i < z; i++)
+    for (j = 0; j < y; j++)
+	  for (k = 0; k < x; k++)
       {
-	A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
-	B[i][j] = ((DATA_TYPE) i*(j+3) + 3) / n;
+	A[i][j][k] = ((DATA_TYPE) 1;
+	B[i][j][k] = ((DATA_TYPE) 0;
       }
 }
 
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
 static
-void print_array(int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+void print_array(int ni, int nj, int nk,
+		 DATA_TYPE POLYBENCH_3D(B,X,Y,Z,x,y,z))
 
 {
-  int i, j;
+  int i, j, k;
 
-  POLYBENCH_DUMP_START;
-  POLYBENCH_DUMP_BEGIN("A");
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0) fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, A[i][j]);
-    }
-  POLYBENCH_DUMP_END("A");
-  POLYBENCH_DUMP_FINISH;
+  for (i = 0; i < x; i++)
+    for (j = 0; j < y; j++)
+      for (k = 0; j < z; k++) {
+	fprintf(stderr, DATA_PRINTF_MODIFIER, B[i][j][k]);
+	if (((i * Y + j) * X + k) % 20 == 0) fprintf(stderr, "\n");
+      }
+  fprintf(stderr, "\n");
 }
 
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
 static
-void kernel_jacobi_2d(int tsteps,
-			    int n,
-			    DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-			    DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+void kernel_jacobi_3d(int tsteps,
+			    int x,
+			    int y,
+			    int z,
+			    DATA_TYPE POLYBENCH_3D(A,X,Y,Z,x,y,z),
+			    DATA_TYPE POLYBENCH_3D(B,X,Y,Z,x,y,z))
 {
-  int t, i, j;
+  int t, i, j,k;
 
 #pragma scop
   for (t = 0; t < _PB_TSTEPS; t++)
     {
-      for (i = 1; i < _PB_N - 1; i++)
-	for (j = 1; j < _PB_N - 1; j++)
-	  B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
-      for (i = 1; i < _PB_N - 1; i++)
-	for (j = 1; j < _PB_N - 1; j++)
-	  A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j-1] + B[i][1+j] + B[1+i][j] + B[i-1][j]);
+      for (i = 1; i < _PB_Z - 1; i++)
+		for (j = 1; j < _PB_X - 1; j++)
+		 for (k = 1; k < _PB_Y - 1; k++)
+			B[i][j][k] =  SCALAR_VAL(0.1) * A[i][j][k]
+						+ SCALAR_VAL(0.1) * A[i][j][k-1]
+						+ SCALAR_VAL(0.1) * A[i][j][k+1]
+						+ SCALAR_VAL(0.1) * A[i][j-1][k]
+						+ SCALAR_VAL(0.1) * A[i][j+1][k]
+						+ SCALAR_VAL(0.1) * A[i+1][j][k]
+						+ SCALAR_VAL(0.1) * A[i-1][j][k];
+			             
+      for (i = 1; i < _PB_Z - 1; i++)
+		for (j = 1; j < _PB_Y - 1; j++)
+			for (k = 1; j < _PB_X - 1; k++)
+				A[i][j][k] = B[i][j][k];
     }
 #pragma endscop
 
@@ -87,22 +99,24 @@ void kernel_jacobi_2d(int tsteps,
 int main(int argc, char** argv)
 {
   /* Retrieve problem size. */
-  int n = N;
+  int x = X;
+  int y = Y;
+  int x = Z;
   int tsteps = TSTEPS;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
+  POLYBENCH_3D_ARRAY_DECL(A, DATA_TYPE, X, Y, Z, x, y, z);
+  POLYBENCH_3D_ARRAY_DECL(B, DATA_TYPE, X, Y, Z, x, y, z);
 
 
   /* Initialize array(s). */
-  init_array (n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+   init_array (x, y, z, POLYBENCH_ARRAY(A));
 
   /* Start timer. */
   polybench_start_instruments;
 
   /* Run kernel. */
-  kernel_jacobi_2d(tsteps, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+  kernel_jacobi_3d(tsteps, x, y, z, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
 
   /* Stop and print timer. */
   polybench_stop_instruments;
@@ -110,7 +124,7 @@ int main(int argc, char** argv)
 
   /* Prevent dead-code elimination. All live-out data must be printed
      by the function call in argument. */
-  polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(A)));
+  polybench_prevent_dce(print_array(x,y,z, POLYBENCH_ARRAY(B)));
 
   /* Be clean. */
   POLYBENCH_FREE_ARRAY(A);
